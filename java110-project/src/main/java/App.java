be5aa8e65b307873b5d6353039f0c339d1eb2987
minsung2da <minsung2da@naver.com>
2018-09-07 09:54:58 +0900

    인터페이스 활용

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+import bitcamp.java110.cms.control.Controller;
 import bitcamp.java110.cms.control.ManagerController;
 import bitcamp.java110.cms.control.StudentController;
 import bitcamp.java110.cms.control.TeacherController;
@@ -13,36 +15,41 @@ import bitcamp.java110.cms.domain.Teacher;
 
 
 public class App {
-    
+
     static Scanner keyIn = new Scanner(System.in);
 
+
     public static void main(String[] args) {
-        
-        StudentController sc = new StudentController(
-                keyIn, new LinkedList<Student>());
-        TeacherController tc = new TeacherController(
-                keyIn,new ArrayList<Teacher>());
-        ManagerController mc = new ManagerController(
-                keyIn, new ArrayList<Manager>());
-        
+
+        HashMap<String,Controller> requestHandlerMapping =  new HashMap<>();
+
+
+        requestHandlerMapping.put("1", 
+                new StudentController(new LinkedList<Student>()));
+        requestHandlerMapping.put("2",
+                new TeacherController(new ArrayList<Teacher>()));
+        requestHandlerMapping.put("3",
+                new ManagerController(new ArrayList<Manager>()));
+
         while (true) {
+
             String menu = promptMenu();
-            
-            if (menu.equals("1")) {
-                sc.serviceStudentMenu();
-                
-            } else if (menu.equals("2")) {
-                tc.serviceTeacherMenu();
-                
-            } else if (menu.equals("3")) {
-                mc.serviceManagerMenu();
-                
-            } else if (menu.equals("0")){
+
+            if (menu.equals("0")){
                 System.out.println("안녕히 가세요!");
                 break;
             }
+
+            Controller controller = requestHandlerMapping.get(menu);
+
+            if(controller!=null)
+            {
+                controller.service(keyIn);
+
+            } 
+
         }
-        
+
         keyIn.close();
     }
 
@@ -52,12 +59,12 @@ public class App {
         System.out.println("2.강사 관리");
         System.out.println("3.매니저 관리");
         System.out.println("0.종료");
-        
+
         while (true) {
             System.out.print("메뉴 번호> ");
-            
+
             String menu = keyIn.nextLine();
-            
+
             switch (menu) {
             case "1":
             case "2":
---
+++ java110-project/src/main/java/bitcamp/java110/cms/control/Controller.java
@@ -0,0 +1,10 @@
+package bitcamp.java110.cms.control;
+
+import java.util.Scanner;
+
+public interface Controller {
+
+    void service(Scanner keyIn);
+    
+    
+}
