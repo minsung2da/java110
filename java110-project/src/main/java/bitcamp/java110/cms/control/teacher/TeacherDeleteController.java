a2bf3be0bb2b1473c8e423fef450a8d54cc4c6d2
minsung2da <minsung2da@naver.com>
2018-09-10 14:34:51 +0900

    v1.6수정

--- java110-project/src/main/java/bitcamp/java110/cms/control/teacher/TeacherDeleteController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/teacher/TeacherDeleteController.java
@@ -14,17 +14,18 @@ public class TeacherDeleteController {
 
     
     @RequestMapping("teacher/delete")
-    private void delete(Scanner keyIn) {
-        System.out.print("삭제할 번호? ");
-        int no = Integer.parseInt(keyIn.nextLine());
-        
-        if (no < 0 || no >= App.teachers.size()) {
-            System.out.println("무효한 번호입니다.");
-            return;
-        }
-        
-        App.teachers.remove(no);
-        
-        System.out.println("삭제하였습니다.");
+public void delete(Scanner keyIn) {
+    System.out.print("삭제할 번호? ");
+    int no = Integer.parseInt(keyIn.nextLine());
+    
+    if (no < 0 || no >=  App.teachers.size()) {
+        System.out.println("무효한 번호입니다.");
+        return;
     }
+    
+    App.teachers.remove(no);
+    
+    System.out.println("삭제하였습니다.");
+}
+
 }
