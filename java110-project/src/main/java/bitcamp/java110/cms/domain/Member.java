f9b38b63bcf575acbda22d3a39254fb6049de716
minsung2da <minsung2da@naver.com>
2018-09-12 12:20:05 +0900

    DAO에 Serialize 적용 및 transient field 적용

--- java110-project/src/main/java/bitcamp/java110/cms/domain/Member.java
+++ java110-project/src/main/java/bitcamp/java110/cms/domain/Member.java
@@ -8,7 +8,9 @@ public class Member implements Serializable{
    
     protected String name;
     protected String email;
-    protected String password;
+    
+    // transient 필드 : serialize 대상에서 제외한다.
+    protected  transient String password;
     
     public String getName() {
         return name;
