72b016aac80d6722be6b32ee900f0561943fcb51
minsung2da <minsung2da@naver.com>
2018-09-13 15:37:25 +0900

    .

---
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/DaoException.java
@@ -0,0 +1,23 @@
+package bitcamp.java110.cms.dao;
+
+public class DaoException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DaoException() {
+        super();
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+    
+    
+    
+    
+
+}
--- java110-project/src/main/java/bitcamp/java110/cms/dao/impl/ManagerJdbcDao.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/impl/ManagerJdbcDao.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bitcamp.java110.cms.annotation.Component;
+import bitcamp.java110.cms.dao.DaoException;
 import bitcamp.java110.cms.dao.ManagerDao;
 import bitcamp.java110.cms.domain.Manager;
 
@@ -22,7 +23,10 @@ public class ManagerJdbcDao implements ManagerDao {
    public List<Manager> findAll(){
        
        ArrayList<Manager> list = new ArrayList<>();
-      
+       Connection con = null;
+       Statement stmt = null;
+       ResultSet rs = null;
+       
      try {  
        //java.sql.Driver 구현체를 로딩한다.
        // 해당 클래스의 객체를 만들어 DriverManager 에 등록한다.
@@ -34,10 +38,10 @@ public class ManagerJdbcDao implements ManagerDao {
        // DriverManager 는 등록된 Driver 들 중에서 요구 사항에 맞는
        // 드라이버를 찾아 connect() 를 호출한다
        // 그리고 connect() 매서드의 리턴 값을 그대로 리턴해 준다.
-       Connection con = DriverManager.getConnection(
+      con = DriverManager.getConnection(
                "jdbc:mariadb://localhost:3306/studydb","study","1111");
         // => 질의문을 작성할 객체를 준비한다.
-       Statement stmt = con.createStatement();
+       stmt = con.createStatement();
         //=> select 질의를 한다.
       
        /* 
@@ -48,7 +52,7 @@ public class ManagerJdbcDao implements ManagerDao {
          
         * */
        
-     ResultSet rs = stmt.executeQuery("select"+
+     rs = stmt.executeQuery("select"+
                          " m.name,"+
                           " m.email,"+
                           " mr.posi "+
@@ -57,7 +61,9 @@ public class ManagerJdbcDao implements ManagerDao {
        // 서버에 생성된 질의 결과를 한개씩 가져온다.
      
      while(rs.next()) {
+         
          Manager mgr = new Manager();
+         
          mgr.setEmail(rs.getString("email"));
          mgr.setName(rs.getString("name"));
          mgr.setPosition(rs.getString("posi"));
@@ -67,7 +73,11 @@ public class ManagerJdbcDao implements ManagerDao {
        
        
      }catch(Exception e) {
-         
+         throw new DaoException(e);
+     }finally {
+         rs.close();
+         stmt.close();
+         con.close();
      }
        
    }
