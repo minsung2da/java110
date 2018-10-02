0c47c2dac0e552d4d2bfd18be1c0e2595d3a491b
minsung2da <minsung2da@naver.com>
2018-10-02 11:43:44 +0900

    HttpSession logout

--- java110-project/src/main/java/bitcamp/java110/cms/servlet/HeaderServlet.java
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/HeaderServlet.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bitcamp.java110.cms.domain.Manager;
 import bitcamp.java110.cms.domain.Member;
+import bitcamp.java110.cms.domain.Teacher;
 
 @WebServlet("/header")
 public class HeaderServlet extends HttpServlet {
@@ -33,6 +35,13 @@ public class HeaderServlet extends HttpServlet {
         if (loginUser == null) {
             out.println("<li><a href='/auth/login'>로그인</a></li>");
         } else {
+            String loginType = "학생";
+            if(loginUser instanceof Manager) {
+                loginType = "매니저";
+            }else if (loginUser instanceof Teacher) {
+                loginType = "강사";
+            }
+            
             out.printf("<li>%s(<a href='/auth/logout'>로그아웃</a>)</li>\n", 
                     loginUser.getName());
         }
---
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/auth/LogoutServlet.java
@@ -0,0 +1,52 @@
+package bitcamp.java110.cms.servlet.auth;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bitcamp.java110.cms.dao.ManagerDao;
+import bitcamp.java110.cms.dao.StudentDao;
+import bitcamp.java110.cms.dao.TeacherDao;
+import bitcamp.java110.cms.domain.Member;
+
+@WebServlet("/auth/loginout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(
+            HttpServletRequest request, 
+            HttpServletResponse response) 
+                    throws ServletException, IOException {
+      
+        HttpSession session = request.getSession();
+        
+        //현재 세션을 무효화 시킨다.
+        session.invalidate();
+        
+        response.sendRedirect("login");
+        
+        
+        
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
